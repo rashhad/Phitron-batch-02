062abeca12f7c77e88e18a1c74dcae0135daaac7
rashhad <rislam139@gmail.com>
2022-12-05 20:26:30 +0600

    26.5 updated

--- modules/module 26.5.c
+++ modules/module 26.5.c
@@ -4,228 +4,99 @@
 #include <string.h>
 #include <stdbool.h>
 
-int nth_element(int n);
-void print(int n);
-int nth_sum(int n);
-void is_cost(char a[], int len);
-int find_nth_lar(int a[],int sz,int n);
-int find_nth_sm(int a[],int n);
-void print_array(int a[], int n);
-void sort(int a[],int n);
-void solve1(void);
-void solve2(void);
-void solve3(void);
-void solve5(void);
-void solve4(void);
-void solve6(void);
-void solve7(void);
-void solve8(void);
-void solve9(void);
-char* pr(int num);
-int count_except(int sz, int ar[],int k);
+double circle_ar(int r);
 int main()
 {
-//    solve1();
-//    solve2();
-//    solve3();
-//    solve4();
-//    solve5();
-//    solve6();
-//    solve7();
-//    solve8();
-    solve9();
+    solve6();
     return 0;
 }
-void solve9(void)
-{
-    int t; scanf("%d",&t);
-    while(t--)
-    {
-        int m,n,o,p;
-        scanf("%d%d%d%d",&m,&n,&o,&p);
-        if((m>=o&&m<=p) || (o<=n&&o>=m))
-            m>=o?printf("%d",m):printf("%d",o);
-        else printf("%d",m+o);
-        printf("\n");
-    }
-}
-void solve8(void)
-{
-    int t;
-    scanf("%d",&t);
-    while(t--)
-    {
-        int n;
-        scanf("%d",&n);
-        int a[n];
-        a[0]=2;
-        for(int i =1; i<n; i++)
-        {
-            a[i]=a[i-1];
-            a[i]++;
-            while(a[i]%a[i-1]==0)
-                a[i]++;
-        }
-        print_array(a,n);
-        printf("\n");
-    }
-}
-void solve7(void)
-{
-    int t;
-    scanf("%d",&t);
-    while(t--)
-    {
-        int n;
-        scanf("%d",&n);
-        printf("%d\n",nth_element(n));
-    }
-}
 
-int nth_element(int n)
+void solve1(void)
 {
-    int k=0;
-    for(int i=0; i<n; i++)
-    {
-        k++;
-        while(k%3==0 || k%10==3)
-            k++;
-    }
-    return k;
-}
-void solve6(void)
-{
-    int a, b, c,* p=&a, *q=&b, *r=&c;
-    scanf("%d%d%d",p,q,r);
-    printf("%d",*p+*q+*r);
-    return;
-}
-void solve5(void)
-{
-    int n;
-    scanf("%d",&n);
-    printf("%d",nth_sum(n));
-    return;
-}
-void solve4(void)
-{
-    int n;
-    scanf("%d",&n);
-    print(n);
-    return;
-}
-void solve3(void)
-{
-    char a[50];
-    scanf("%s",a);
-    is_cost(a,strlen(a));
-    return;
+    int x, y;
+    int* px=&x, *py=&y;
+
+    scanf("%d%d",px,py);
+    printf("%.3f",(*px+*py)/2.0);
 }
 void solve2(void)
 {
-    //finding kth element;
-    int n;
-    scanf("%d",&n);
-    int num[n];
-    for(int i = 0; i<n; i++)
-        scanf("%d",&num[i]);
-    sort(num,n);
-    int k;
-    scanf("%d",&k);
-    printf("%d%s largest element is: %d ",k,pr(k), find_nth_lar(num,n,k));
-    printf("\n%d%s smallest element is: %d ",k,pr(k), find_nth_sm(num,k));
-    return;
-}
-void solve1(void)
-{
-    //counting total array element except... ;
-    int n;
-    scanf("%d",&n);
-
+    int n; scanf("%d",&n);
     int a[n];
-    while(n--)
-    {
-        scanf("%d",&a[n]);
-    }
-    int k;
-    scanf("%d", &k);
-    printf("%d",count_except(sizeof(a)/sizeof(a[0]),a,k));
-    return;
-}
-int count_except(int sz, int ar[],int k)
-{
-    int count=0;
-    for(int i =0; i<sz; i++)
-        if(ar[i]!=k)
-            count++;
-    return count;
-}
 
-void sort(int a[],int n)
-{
-    int select;
-    for(int i = 1; i<n; i++)
-    {
-        select=a[i];
-        int j = i-1;
-        while(select<a[j]&&j>=0)
-        {
-            a[j+1]=a[j];
-            j--;
-        }
-        if(j<0)
-            a[++j]=select;
-        else
-            a[j+1]=select;
-    }
-}
-
-void print_array(int a[], int n)
-{
     for(int i = 0; i<n; i++)
-        printf("%d ",a[i]);
-}
-int find_nth_sm(int a[],int n)
-{
-    return a[n-1];
-}
-int find_nth_lar(int a[],int sz,int n)
-{
-    return a[sz-n];
-}
-
-char* pr(int num)
-{
-    if(num%10==3)
-        return "rd";
-    else if(num%10==2)
-        return "nd";
-    else if(num%10==1)
-        return "st";
-    return "th";
+        scanf("%d",&a[i]);
+    for(int i = 0; i<n-1; i++)
+        for(int j = 0; j<n-1-i;j++)
+            if(a[j]>a[j+1])
+    {
+        swapp(&a[j],&a[j+1]);
+    }
+    print_arr(a,n);
 }
 
-void is_cost(char a[], int len)
+void solve3(void)
 {
-    int sum=0;
-    for(int i =0; i<len; i++)
-        sum+=a[i]-'a'+1;
-    while(sum%2==0)
-        sum/=2;
-    printf("%s",sum>1?"No":"Yes");
+    int n; scanf("%d",&n);
+    int a[n];
+    for(int i = 0; i<n; i++)
+        scanf("%d",&a[i]);
+    print_arr(a,n);
+}
+void solve4(void)
+{
+    int n; scanf("%d",&n);
+    printf("%lf sqm",circle_ar(n));
+}
+void solve5(void)
+{
+    int n; scanf("%d",&n);
+    int a[n];
+    for(int i = 0; i<n; i++)
+        scanf("%d",&a[i]);
+    printf("%d",sum_last_zero(a,n));
 }
 
-void print(int n)
+void solve6(void)
 {
-    printf("%d",n);
-    if(n>1)
-        print(n-1);
-    printf("%d",n);
+    char a[100];
+    scanf("%s",&a);
+
+    capitilize_even_char(a,strlen(a));
+    printf("%s",a);
+}
+void swapp(int *x, int* y)
+{
+    int t;
+    t = *x;
+    *x=*y;
+    *y=t;
 }
 
-int nth_sum(int n)
+void print_arr(int* arr,int size)
 {
-    if(n>1)
-        return n+nth_sum(n-1);
-    return 1;
+    for(int* ptr = arr; ptr<arr+size; ptr++)
+        printf("%d ",*(ptr));
+    printf("\n");
+}
+
+double circle_ar(int r)
+{
+    return 3.1416*r*r;
+}
+
+int sum_last_zero(int a[],int size)
+{
+    int summ=0;
+    for(int i = 0; i<size; i++)
+        if(a[i]%10==0)
+            summ+=a[i];
+    return summ;
+}
+
+void capitilize_even_char(char arr[], int len)
+{
+    for(int i = 0; i<len; i++)
+        if(arr[i]%2==0)
+            arr[i]-=32;
 }
